da4775761a860f894131891508240382339ae94d
Nicolas Guérin <nicolas.guerin03@gmail.com>
2024-08-05 16:11:23 +0200

    C2 - MongoDB connection problem

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-const express = require("express")
-
+const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
+mongoose.connect('mongodb+srv://NicolasAdminTest:<kmxpSg8KaR9FYD2G>@cluster0.takpkux.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+        .then(() => console.log('Connection à MongoDB réussie !'))
+        .catch(() => console.log('Connection à MongoDB échouée !'))
+    
+
+
+// Permet d'extraire le body Json des requêtes POST provenant du Front-End
+// Il prend toutes les requêtes qui ton comme content-Type application/json
+app.use(express.json())
 
 app.use((req, res, next) => {
-    console.log("Requête reçue !")
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
     next()
-})
+  })
 
-app.use((req, res, next) => {
-    res.status(201)
-    next()
-})
+app.post('/api/auth/signup', (req, res, next) => {
+    console.log(req.body)
+    res.status(201).json({
+        message: 'Objet créé !'
+    })
+} )
 
-app.use((req, res, next) => {
-    res.json({ message: "Votre requête a bien été reçue !"})
-    next()
-})
-
-app.use((req, res) => {
-    console.log("Réponse envoyée avec succès !")
-})
 
 module.exports = app
